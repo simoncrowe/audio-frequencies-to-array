0cfe5ee5f6f2f73901a7168ba0641e4f70fd8b42
Simon Crowe <info@simoncrowe.net>
2020-05-02 17:13:46 +0100

    Fixed logic for phase offset.

--- utils.py
+++ utils.py
@@ -1,24 +1,18 @@
 import numpy as np
 
 
-def generate_sine_wave(freq, duration, gain=0.5, phase_offset=0, sample_rate=44100):
+def generate_sine_wave(freq, duration, amp=0.5, phase_offset=0, sample_rate=44100):
     phase_duration = freq / sample_rate
     samples_per_phase = sample_rate / freq
-    num_samples = (sample_rate * duration) + samples_per_phase
+    num_samples = (sample_rate * duration) + (3 * samples_per_phase)
     
     sine_samples = np.sin(2 * np.pi * np.arange(num_samples) * phase_duration)
-    sine_samples *= gain
+    sine_samples *= amp
     
     samples_per_phase = sample_rate / freq
-    start_slice = int(samples_per_phase* phase_offset)
-    end_slice = int(samples_per_phase * (1 - phase_offset))
+    start_slice = int(samples_per_phase + (samples_per_phase * phase_offset))
+    end_slice = int(samples_per_phase + (samples_per_phase * (1 - phase_offset)))
     
-    if phase_offset not in (1, 0):
-        end_slice += 1
-
-    if end_slice == 0:
-        return sine_samples.astype(np.float32)[start_slice:]
-    else:
-        return sine_samples.astype(np.float32)[start_slice: - end_slice]
+    return sine_samples.astype(np.float32)[start_slice: - end_slice]
 
 
